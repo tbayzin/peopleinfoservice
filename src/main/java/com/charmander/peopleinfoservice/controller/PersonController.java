dbb8ec5947e2ac0c6b6ce183ff5792ab7d3c3441
tarikb <bayzin@hotmail.com.tr>
2021-12-22 17:26:58 +0300

    PersonController değişiklik yapıldı DW

--- src/main/java/com/charmander/peopleinfoservice/controller/PersonController.java
+++ src/main/java/com/charmander/peopleinfoservice/controller/PersonController.java
@@ -1,7 +1,10 @@
 package com.charmander.peopleinfoservice.controller;
 
+import com.charmander.peopleinfoservice.PersonEndpoint;
 import com.charmander.peopleinfoservice.entity.Person;
 import com.charmander.peopleinfoservice.service.PersonService;
+import com.github.tbayzin.peopleinfoservice.GetPersonsoapRequest;
+import com.github.tbayzin.peopleinfoservice.GetPersonsoapResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/people")
 public class PersonController {
 
+    private  final PersonEndpoint personEndpoint;
 private final PersonService personService;
 
+
+
+
 // Buraya bir GET mapping yazılacak  buraya attığın tckn soapdaki tckn sorgulayan endpointe gidecek o da dummy data & db ye gidecek
+
+
+    @GetMapping()
+    public GetPersonsoapResponse getPersonsoapByTckn (@RequestParam GetPersonsoapRequest request) {
+        return personEndpoint.getPerson(request);
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping()
